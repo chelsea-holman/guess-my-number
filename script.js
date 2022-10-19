7c5a19671df30e613633e355f249380b4c323f3b
Chelsea Holman <chel.holman@gmail.com>
2022-10-20 07:18:50 +0800

    implemented scoring logic

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-"use strict";
+'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫 No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      '🎉 You guessed correctly!';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '⬇️ guess lower!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😭 you lost the game!';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '⬆️ guess higher!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😭 you lost the game!';
+    }
+  }
+});
